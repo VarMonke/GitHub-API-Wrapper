301c9c9c424eb3c41faa2cbf4e97d84c30f29d82
sudosnok <subsonic96@gmail.com>
2022-03-26 18:17:45 +0000

    Added a skeleton class for the main entrypoint

--- Github/main.py
+++ Github/main.py
@@ -1,6 +1,15 @@
 #== main.py ==#
 
+__all__ = (
+    'Github',
+)
+
 import aiohttp
-import asyncio
 
 from . import http
+
+class Github:
+    async def start(self):
+        """Main entry point to the wrapper, this creates the ClientSession."""
+        headers = {}
+        self._session = http.make_session(headers=headers)
