669f72e46762577744279cbdd22b8438b024a4e3
sudosnok <subsonic96@gmail.com>
2022-03-27 14:53:12 +0100

    Might've fixed imports?

--- Github/__init__.py
+++ Github/__init__.py
@@ -1,6 +1,10 @@
 #== __init__.py ==#
 
 from .main import *
+from .objects import *
+from .http import *
+from .urls import *
+from .exceptions import *
 
 __title__ = 'Github-Api-Wrapper'
 __authors__ = 'VarMonke', 'sudosnok'
