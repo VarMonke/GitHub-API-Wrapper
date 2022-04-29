eb9567e039b5fed77ebbc974cb9327097c647a05
Vish M <wishymovies@gmail.com>
2022-04-29 16:51:15 +0100

    Update __init__.py

--- Github/__init__.py
+++ Github/__init__.py
@@ -1,11 +1,12 @@
 #== __init__.py ==#
 
+__title__ = 'Github-Api-Wrapper'
+__authors__ = 'VarMonke', 'sudosnok'
+__version__ = '1.0.0'
+
 from .main import *
 from .objects import *
 from .http import *
 from .urls import *
 from .exceptions import *
 
-__title__ = 'Github-Api-Wrapper'
-__authors__ = 'VarMonke', 'sudosnok'
-__version__ = '1.0.0'
