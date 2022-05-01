f65a05beb7f04d8c8c5c47eddb891177b5b398d9
sudosnok <subsonic96@gmail.com>
2022-05-01 18:10:02 +0100

    Added asset

--- docs/conf.py
+++ docs/conf.py
@@ -53,6 +53,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # a list of builtin themes.
 #
 html_theme = 'alabaster'
+html_favicon = './assets/Github-Python.png'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
