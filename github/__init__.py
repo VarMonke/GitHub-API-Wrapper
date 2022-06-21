f03fe71fd6a47f47f6238ddc50f8800058808781
RGBCube <78925721+RGBCube@users.noreply.github.com>
2022-06-21 19:02:04 +0300

    Fix copyright year and format

--- github/__init__.py
+++ github/__init__.py
@@ -4,7 +4,7 @@ __title__ = 'Github-Api-Wrapper'
 __authors__ = 'VarMonke', 'sudosnok'
 __version__ = '1.2.1'
 __license__ = 'MIT'
-__copyright__ = 'Copyright (c) 2020 VarMonke & sudosnok'
+__copyright__ = 'Copyright (c) 2022-present VarMonke & sudosnok'
 
 from .client import *
 from .objects import *
