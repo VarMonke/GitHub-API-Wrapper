cdffbd8843f8603319b0bfb1aa16cf8a218b7a97
RGBCube <78925721+RGBCube@users.noreply.github.com>
2022-06-22 17:34:58 +0300

    make object not require response

--- github/objects/object.py
+++ github/objects/object.py
@@ -1,15 +1,14 @@
-from typing import TYPE_CHECKING, Any, Dict
+from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from ..internals import HTTPClient
 
 
 class Object:
-    __slots__ = ("_response", "_http")
+    __slots__ = ("__http",)
 
-    def __init__(self, response: Dict[str, Any], http: HTTPClient) -> None:
-        self._http = http
-        self._response = response
+    def __init__(self, *, http: HTTPClient) -> None:
+        self.__http = http
 
     def __repr__(self) -> str:
         return f"<{self.__class__.__name__}>"
