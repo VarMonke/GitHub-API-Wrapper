ffb5dfdd24d9863eee67dec37a6fb68391b3f1a5
RGBCube <78925721+RGBCube@users.noreply.github.com>
2022-06-22 15:02:32 +0300

    make the library only http and increase version

--- github/__init__.py
+++ github/__init__.py
@@ -2,10 +2,8 @@
 
 __title__ = "Github-Api-Wrapper"
 __authors__ = "VarMonke", "sudosnok"
-__version__ = "1.2.1"
+__version__ = "2.0a"
 __license__ = "MIT"
 __copyright__ = "Copyright (c) 2022-present VarMonke & sudosnok"
 
-from .client import *
-from .errors import *
-from .objects import *
+from . import http
